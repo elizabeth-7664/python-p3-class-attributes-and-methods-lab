9b79412869bb399329074990eefe47515116db77
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-05-24 16:21:36 +0300

    using class attributes and methods to write durable and powerful code

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Count total songs
+        Song.count += 1
+
+        # Track all genres
+        Song.genres.append(genre)
+        # Track all artists
+        Song.artists.append(artist)
+
+        # Count genre occurrences
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Count artist occurrences
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
